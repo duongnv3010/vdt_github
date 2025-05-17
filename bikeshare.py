3d9d610a7a000d3da22fd1f76fc04e69599f78b8
duongnv3010 <nguyenduong20053010@gmail.com>
2025-05-17 12:02:00 +0700

    add comment for time_stats() function

--- bikeshare.py
+++ bikeshare.py
@@ -122,6 +122,8 @@ def load_data(city, month, day):
 
     return df
 
+# Display statistics on the most frequent times of travel
+
 
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
